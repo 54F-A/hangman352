d9a1e770726473da95a1ffde783f4a6cf038d39d
54F-A <sarfaraj-ahmed@outlook.com>
2024-05-14 16:42:15 +0100

    Milestone 3 - Code Refactoring

--- milestone_3.py
+++ milestone_3.py
@@ -33,29 +33,28 @@ else:
 
 #%%
 
-# Define a function called check_guess. pass in the guess as a parameter for the function.
-def check_guess(guess):
+# Define a function. pass in the guess as a parameter for the function.
+def check_guess(guessed_letter):
 
 # Convert the guess into lower case.
-    guess = guess.lower()
+    guessed_letter = guessed_letter.lower()
     
-    if guess in secret_word:
-        print(f"Good guess! '{guess}' is in the word.")
+    if guessed_letter in secret_word:
+        print(f"Good guess! '{guessed_letter}' is in the word.")
     else:
-        print(f"Sorry, '{guess}' is not in the word. Try again.")
+        print(f"Sorry, '{guessed_letter}' is not in the word. Try again.")
 
 # Define a function called ask_for_input.
 def ask_for_input():
 
     while True:
-        guess = input("Guess a letter: ")
-        if len(guess) == 1 and guess.isalpha():
+        guessed_letter = input("Guess a letter: ")
+        if len(guessed_letter) == 1 and guessed_letter.isalpha():
             break
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
 
 # Outside the while loop, but within this function, call the check_guess function to check if the guess is in the word. 
-    check_guess(guess)
-    
+    check_guess(guessed_letter)
 # Outside the function, call the ask_for_input function to test your code.
 ask_for_input()
