04d14873ef1ea959c62b9db92630d00636579225
54F-A <sarfaraj-ahmed@outlook.com>
2024-05-14 15:46:20 +0100

    Milestone 2 - Code Refactoring

--- milestone_2.py
+++ milestone_2.py
@@ -2,33 +2,29 @@ import random
 
 # Create a list containing the names of your 5 favorite fruits.
 
-list = ["Apple", "Banana", "Grapes", "Orange", "Strawberry"]
-
-# Assign this list to a variable called word_list.
-
-word_list = list
+list_of_fruit = ["Apple", "Banana", "Grapes", "Orange", "Strawberry"]
 
 # Print out the newly created list to the standard output (screen).
 
-print(word_list)
+print(list_of_fruit)
 
-# Create the random.choice method and pass the word_list variable into the choice method.
-# Assign the randomly generated word to a variable called word.
+# Create the random.choice method and pass the variable into the choice method.
+# Assign the randomly generated word to a variable.
 
-word = random.choice(word_list)
+chosen_fruit = random.choice(list_of_fruit)
 
 # Print out word to the standard output.
 
-print(word)
+print(chosen_fruit)
 
 # Using the input function, ask the user to enter a single letter.
-# Assign the input to a variable called guess.
+# Assign the input to a variable.
 # Create an if statement that checks if the length of the input is equal to 1 and the input is alphabetical.
 # In the body of the if statement, print a message that says "Good guess!".
 # Create an else block that prints "Oops! That is not a valid input." if the preceding conditions are not met.
 
-guess = input("Enter a single letter: ")
-if len(guess) == 1 and guess.isalpha():
+chosen_letter = input("Enter a single letter: ")
+if len(chosen_letter) == 1 and chosen_letter.isalpha():
     print("Good Guess!")
 else:
     print("Oops! That is not a valid input.")
